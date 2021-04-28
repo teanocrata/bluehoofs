c38e7a07a65dc4593077644e1974af0c6bc0e1e0
teanocrata <teanocrata@gmail.com>
2021-04-28 21:16:51 +0200

    refactor: :recycle: Use useEffect & convert ServiceCard into a functional component

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { observer } from 'mobx-react';
 import { HoofBluetoothService } from '../devices/HoofBluetoothService';
@@ -9,27 +9,21 @@ interface Props {
 	service: HoofBluetoothService;
 }
 
-export const ServiceCard = observer(
-	class ServiceCard extends React.Component<Props> {
-		componentDidMount() {
-			this.props.service.setCharacteristics();
-		}
-		render() {
-			const { service } = this.props;
-
-			return (
-				<Card style={{ width: '21rem' }}>
-					<CardPrimaryAction>
-						{service.name}
-						{(service.characteristics || []).map(characteristic => (
-							<CharacteristicCard
-								key={characteristic.uuid}
-								characteristic={characteristic}
-							/>
-						))}
-					</CardPrimaryAction>
-				</Card>
-			);
-		}
-	}
-);
+export const ServiceCard = observer(({ service }: Props) => {
+	useEffect(() => {
+		service.setCharacteristics();
+	});
+	return (
+		<Card style={{ width: '21rem' }}>
+			<CardPrimaryAction>
+				{service.name}
+				{(service.characteristics || []).map(characteristic => (
+					<CharacteristicCard
+						key={characteristic.uuid}
+						characteristic={characteristic}
+					/>
+				))}
+			</CardPrimaryAction>
+		</Card>
+	);
+});
